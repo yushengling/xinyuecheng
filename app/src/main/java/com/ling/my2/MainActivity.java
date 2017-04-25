34682981d2f805c3f21800f4626c565b44287310
yushengling <1121513947@qq.com>
2017-04-25 23:12:46 +0800

    修改toast

--- app/src/main/java/com/ling/my2/MainActivity.java
+++ app/src/main/java/com/ling/my2/MainActivity.java
@@ -2,9 +2,8 @@ package com.ling.my2;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,7 +11,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -27,7 +28,7 @@ public class MainActivity extends AppCompatActivity
         onc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+Toast.makeText(MainActivity.this, "d", Toast.LENGTH_SHORT).show();
             }
         });
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
