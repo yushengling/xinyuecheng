3888da628528ff275b8f4d890f79a10f2356f071
yushengling <1121513947@qq.com>
2017-04-25 23:49:17 +0800

    Merge branch 'fenzhi1' into fenzhi
    # Conflicts:
    #	app/src/main/java/com/ling/my2/MainActivity.java

--- app/src/main/java/com/ling/my2/MainActivity.java
+++ app/src/main/java/com/ling/my2/MainActivity.java
@@ -2,8 +2,6 @@ package com.ling.my2;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
